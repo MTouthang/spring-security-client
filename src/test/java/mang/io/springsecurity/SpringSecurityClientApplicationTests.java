8e44d65cd7914701ce6f9b987c30c68af8f4ee07
Mamang touthang <huebart6@gmail.com>
2022-04-04 13:44:53 +0530

    Initial commit-with mysql properties setup

---
+++ src/test/java/mang/io/springsecurity/SpringSecurityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mang.io.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
